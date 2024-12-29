3135914ede4055fbe132fc5d35ff6d8d1d987852
wassim-oucouc <wassimoucouc03@gmail.com>
2024-12-29 15:45:31 +0100

    Update function.php

--- function.php
+++ function.php
@@ -73,20 +73,5 @@ class Contactify {
 }
 
 
-$contactify = new Contactify();
-$contactify->connect();
 
-$contactify->insert("John", "Doe", "john.doe@example.com", "123456789");
-
-// Fetch all contacts
-$contacts = $contactify->fetch();
-echo "<pre>";
-print_r($contacts);
-echo "</pre>";
-
-// Update a contact with ID 1
-$contactify->update(1, "John", "Doe", "john.doe@example.com", "987654321");
-
-// Delete a contact with ID 1
-$contactify->delete(1);
 ?>
